4041000d8478870005991d6fe6e51246dc504515
JJY <ddglackrp@gmail.com>
2024-11-02 16:13:01 +0900

    feat "Lotto 객체 기능 추가"

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,8 @@
 package lotto;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -14,7 +16,21 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
-    }
 
+        for (Integer number : numbers) {
+            if(number < 1 || number > 45){
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45사이어야 합니다.");
+            }
+        }
+
+        Set<Integer> numbersSet = new HashSet<>(numbers);
+        if(numbers.size() != numbersSet.size()){
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 서로 다른 6개여야 합니다.");
+        }
+    }
     // TODO: 추가 기능 구현
+
+    public List<Integer> getNumbers() {
+        return List.copyOf(numbers);
+    }
 }
--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -22,4 +23,27 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @DisplayName("로또 번호가 1부터 45사이가 아니면 예외가 발생한다.")
+    @Test
+    void 로또_번호가_범위를_벗어나면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 48)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 로또 번호는 1부터 45사이어야 합니다.");
+    }
+
+    @DisplayName("로또 번호를 알려준다.")
+    @Test
+    void 로또_번호를_알려준다() {
+        //given
+        Lotto testLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+
+        //expected
+        assertThat(testLotto.getNumbers().contains(1)).isTrue();
+        assertThat(testLotto.getNumbers().contains(2)).isTrue();
+        assertThat(testLotto.getNumbers().contains(3)).isTrue();
+        assertThat(testLotto.getNumbers().contains(4)).isTrue();
+        assertThat(testLotto.getNumbers().contains(5)).isTrue();
+        assertThat(testLotto.getNumbers().contains(6)).isTrue();
+        assertThat(testLotto.getNumbers().contains(7)).isFalse();
+    }
 }
